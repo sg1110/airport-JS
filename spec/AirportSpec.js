f48468acaf1cea467f2671e74ee005d258e4498c
sg1110 <saule.guzyte@tesco.com>
2019-02-05 16:24:53 +0000

    Prevents plane from taking off if stormy+error+tests

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -2,31 +2,51 @@ describe("Airport", function() {
   var airport;
 
   beforeEach(function() {
-    airport = new Airport();
-
+    weather = new Weather();
+    spyOn(weather,'is_it_stormy').and.returnValue(false);
+    airport_sunny = new Airport(weather);
   });
 
   it("should be able to land a plane at the airport", function() {
-    airport.land("plane");
-    expect(airport.planes_landed).toEqual(["plane"]);
+    airport_sunny.land("plane");
+    expect(airport_sunny.planes_landed).toEqual(["plane"]);
   });
 
   it("should be able to instruct a plane to take off", function() {
-    airport.land("plane")
-    airport.take_off("plane")
-    expect(airport.planes_landed).not.toEqual(["plane"])
-    expect(airport.planes_landed).toEqual([])
+    airport_sunny.land("plane")
+    airport_sunny.take_off("plane")
+    expect(airport_sunny.planes_landed).not.toEqual(["plane"])
+    expect(airport_sunny.planes_landed).toEqual([])
   });
 
   it("should be able to instruct a plane to take off", function() {
-    airport.land("plane")
-    airport.land("plane2")
-    airport.take_off("plane")
-    expect(airport.planes_landed).toEqual(["plane2"])
+    airport_sunny.land("plane")
+    airport_sunny.land("plane2")
+    airport_sunny.take_off("plane")
+    expect(airport_sunny.planes_landed).toEqual(["plane2"])
   });
 
-  it("should confirm that plane has left the airport", function() {
-    airport.land("plane")
-    expect(airport.take_off("plane")).toEqual("Plane plane has left the airport")
+  it("should confirm that plane has left the airport_sunny", function() {
+    airport_sunny.land("plane")
+    expect(airport_sunny.take_off("plane")).toEqual("Plane plane has left the airport");
   });
+
+
+});
+
+describe("Airport", function() {
+  var airport;
+
+  beforeEach(function() {
+    weather = new Weather();
+    spyOn(weather,'is_it_stormy').and.returnValue(true);
+
+    airport_stormy = new Airport(weather);
+});
+
+  it("should throw an error because weather is stormy", function() {
+    airport_stormy.land("plane")
+    expect(function(){airport_stormy.take_off("plane")}).toThrow(("It is currently stormy - No Go!"));
+  });
+
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,12 +1,27 @@
-function Airport () {
+function Airport (weather = new Weather) {
   this.planes_landed = []
+  this.weather = weather
 }
 Airport.prototype.land = function(plane) {
   this.planes_landed.push(plane)
 }
 
 Airport.prototype.take_off = function(plane) {
-  this.planes_landed = this.planes_landed.filter(function(landed_plane) {
-    return landed_plane != plane;
-  }); return ("Plane " + plane + " has left the airport")
-}
+
+  if (this.weather.is_it_stormy() === true)
+    {throw "It is currently stormy - No Go!"}
+
+  else
+    {this.planes_landed = this.planes_landed.filter(function(landed_plane) {
+      return landed_plane != plane;
+    }); return ("Plane " + plane + " has left the airport")}
+  }
+
+
+  // if (this.weather.is_it_stormy() === false)
+  // {this.planes_landed = this.planes_landed.filter(function(landed_plane) {
+  //   return landed_plane != plane;
+  // }); return ("Plane " + plane + " has left the airport")}
+  // else
+  // {return "whatsup"
+  // throw "It is currently stormy - No Go!"}
